0e1cd38a5cbd75129d4705aa466e06c51bb31e84
pythagodzilla <i@pythagodzilla.top>
2025-10-09 14:10:43 +0000

    footer

---
+++ frontend/src/components/indexBasicFooter.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+<template>
+    <mdui-divider></mdui-divider>
+
+    <div class="flex flex-row justify-center items-center m-4">
+        <p class="text-sm dark:text-gray-400 text-gray-800 m-4">{{$t('navbar.identifyInfo')}}</p>
+        <a href="https://beian.miit.gov.cn/" target="_blank" class="text-sm dark:text-gray-400 text-gray-800">京ICP备11111</a>
+    </div>
+</template>
